81ca6e16604930268996a11d617178ecd9a71dbc
Jimmy Smith <jimmysmith@Jimmys-MacBook-Pro.local>
2020-04-11 15:27:38 -0500

    added blog templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = { 'username': 'Jimmy' }
+    posts = [
+        {
+            'author': {'username': 'Jimmy'},
+            'body': 'Beautiful day in Chicago!'
+        },
+        {
+            'author': {'username': 'Goddess'},
+            'body': 'Jimmy is my husband and who I look up to!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
